e4b91c91ee546f4fc813ed480988538b34a955af
Pavel <lpsyl888l@yandex.ru>
2017-01-27 11:02:23 +0200

    style gallery add magnific PopUp

--- gym/js/common.js
+++ gym/js/common.js
@@ -127,8 +127,29 @@ function slide (sel, q){
 		}
 	});
 }
+//NOT INDEX PAGE
+if(!/index/.test(window.location.href)){
+// console.log($(location).attr('href'));
 
-	$("#bearGal").unitegallery({
-		tiles_type:"justified"
-	}); 
+}
+
+
+//magnific popup  GALLERY============
+$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
 });
